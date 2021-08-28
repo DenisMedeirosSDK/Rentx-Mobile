6ddcbdbc9aa9bd72bc719da971c193ebc13bf7c0
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-08-28 09:44:41 -0300

    fix: consume total cars

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -73,7 +73,7 @@ export function Home() {
         <Header>
           <HeaderContent>
             <Logo width={RFValue(108)} height={RFValue(12)} />
-            <TotalCars>Total de 21 carros</TotalCars>
+            <TotalCars>Total de {cars.length} carros</TotalCars>
           </HeaderContent>
         </Header>
         {isLoading ? (
