46c0f05658cef5f083bb198366d9880e564230cc
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-08-28 17:18:10 -0300

    refactory: changes props button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,8 +2,12 @@ import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
-interface ButtonProps extends RectButtonProps {
+interface ButtonProps {
   color?: string;
+}
+
+interface ButtonTextProps {
+  light?: boolean;
 }
 
 export const Container = styled(RectButton)<ButtonProps>`
@@ -11,13 +15,14 @@ export const Container = styled(RectButton)<ButtonProps>`
   padding: 19px;
   align-items: center;
   justify-content: center;
-
+  margin-bottom: 8px;
   background-color: ${({ color, theme }) =>
     color ? color : theme.colors.main};
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<ButtonTextProps>`
   font-family: ${({ theme }) => theme.fonts.primary_500};
-  color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme, light }) =>
+    light ? theme.colors.header : theme.colors.shape};
   font-size: ${RFValue(15)}px;
 `;
